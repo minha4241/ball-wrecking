ab5ca873ec78d94698b4d0c7e2ae2cb0ff19730f
minha4241 <73830261+minha4241@users.noreply.github.com>
2021-06-06 12:13:09 +0530

    Add files via upload

---
+++ MatterJSBoilerPlate-master/sketch.js
@@ -0,0 +1,47 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+
+
+
+var ground1
+
+
+function preload()
+{
+	
+
+
+
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+     //ground1= new ground(400,680, 800, 20)
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  rect(100,100,100,10)
+  drawSprites();
+  //ground1.display()
+ 
+}
+
+
+
